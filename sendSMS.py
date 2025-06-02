2147d3e536433af383e5b2e2ebccceb50127217a
Sham Ganesh <123235630+sham-arch@users.noreply.github.com>
2025-06-03 02:04:48 +0530

    Update sendSMS.py

--- sendSMS.py
+++ sendSMS.py
@@ -1,26 +1,22 @@
 from twilio.rest import Client
+import os
+from dotenv import load_dotenv
 
-#authentication
+# Load environment variables from .env file
+load_dotenv()
+
 def send(msg):
-    account_sid = 'AC86c6334ae38973ad83e7d262feb2fc66'
-    auth_token = 'a7385fb854f5e30ae41923c8a74846a7'
-    twilio_phone_number = '+15642323524'
+    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+    twilio_phone_number = os.getenv('TWILIO_PHONE_NUMBER')
+    recipient_phone_number = os.getenv('RECIPIENT_PHONE_NUMBER')
 
-    # Recipient's phone number
-    recipient_phone_number = '+919585559723'  # Replace with the actual phone number
-
-    
-    # Initialize the Twilio client
     client = Client(account_sid, auth_token)
 
-    # Send the SMS
     message = client.messages.create(
         to=recipient_phone_number,
         from_=twilio_phone_number,
         body=msg
     )
 
-    # Print the SID (unique identifier) of the sent message
     print(f"Message SID: {message.sid}")
-        
-    
